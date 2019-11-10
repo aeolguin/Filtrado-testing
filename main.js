6a76ab1e5303599228f0715518119b3e6341bcf4
aeolguin <aredolguin@gmail.com>
2019-11-10 12:53:13 -0300

    avanzando

---
+++ main.js
@@ -0,0 +1,3 @@
+var formulario = document.getElementById("main");
+var datosFormulario = "<form name='Marcas' action='#'> <input type='text' id='marca'> </input> <input type='submit' value='Submit'></input> </form>";
+formulario.innerHTML = datosFormulario;
---
+++ valores.js
@@ -0,0 +1,6 @@
+var marcas = ["FORD", "CHEVROLET", "RENAULT", "PEUGEOT"];
+var modelosFord = ["KA", "FIESTA", "FOCUS", "MUSTANG"];
+var modelosChevy = ["S10", "SPIN", "CORSA", "CAMARO"];
+var modelosRanault = ["KWID", "DUSTER", "CAPTURE", "SANDERO"];
+var modelosPeugeot = ["207", "308", "3008", "RC"];
+
